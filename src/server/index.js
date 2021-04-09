ac88101713ee1f2b9f0c7a583f0b8853b01aafe4
Arthur Henrique <arthurhenrique@live.com>
2021-04-09 00:43:47 -0300

    📦 Implementação de desconexão e movimentação rudimentar

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,44 @@
 import express from 'express'
 import http from 'http'
 import { Server } from 'socket.io'
+import Player from './Player.js'
 
 const app = express()
 const webserver = http.createServer(app)
 const io = new Server(webserver, {cors: {origin: '*'}})
 
+const players = []
 io.on('connection', function (socket) {
-  console.log(`Jogador ${socket.id} acabou de se conectar.`)
-  socket.emit('connected', `Você acabou de se conectar com o id ${socket.id}`)
+  
+  players.push(new Player(socket.id))
+  io.emit('refreshPlayersState', players)
+
+  socket.on('disconnect', () => {
+    for (let i = 0; i < players.length; i++) {
+      if (players[i].socketId != socket.id) {
+        continue
+      }
+
+      players.splice(i, 1)
+      io.emit('refreshPlayersState', players)
+
+      break
+    }
+  })
+
+  socket.on('move', (position) => {
+    for (const i = 0; i < players.length; i++) {
+      if (players[i].socketId != socket.id) {
+        continue
+      }
+
+      players[i].setPosition(position.X, position.Y, position.Z)
+
+      break;
+    }
+
+    io.emit('refreshPlayersState', players)
+  })
 })
 
 webserver.listen(3000, function() {

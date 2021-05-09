35b0e8713cd073a7c3512d3c25226b5c2bbe870f
Giseudo Oliveira <giseudo.oliveira@convenia.com.br>
2021-05-08 23:29:54 -0300

    Reverted scene modifications

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -2,25 +2,6 @@
   <div class="game">
     <router-view></router-view>
 
-    <g-hud>
-      <template v-slot:top>
-        <span class="visor visor--top" />
-      </template>
-
-      <template v-slot:down>
-        <span class="visor visor--down" />
-      </template>
-
-      <template v-slot:left>
-        <img class="vector" :src="require('@/assets/vector.svg')" />
-      </template>
-
-      <template v-slot:right>
-        <span class="square" v-for="square in 20" />
-        <span class="line" />
-      </template>
-    </g-hud>
-
     <g-touch-axis @move="onTouchChange" />
     <g-dialogue v-if="showDialogue" />
   </div>
@@ -28,7 +9,7 @@
 
 <script>
 import { markRaw } from 'vue'
-import { GTouchAxis, GDialogue, GHud } from '@/ui'
+import { GTouchAxis, GDialogue } from '@/ui'
 import { Entity, subscribe, unsubscribe } from '@/engine'
 import { PlayerEntity, SkyboxEntity } from '@/entities'
 import { Movement } from '@/components'
@@ -40,7 +21,7 @@ export default {
 
   inject: [ 'input', 'camera', 'network' ],
 
-  components: { GTouchAxis, GDialogue, GHud },
+  components: { GTouchAxis, GDialogue },
 
   data: () => ({
     entities: markRaw([]),
@@ -88,7 +69,7 @@ export default {
       )
       this.entities.push(entity)
 
-      this.camera.position.set(0, 1.5, 8)
+      this.camera.position.set(0, 4, 15)
       this.camera.position.add(entity.position)
       this.camera.lookAt(entity.position)
       this.camera.follow(entity)
@@ -126,57 +107,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss">
-.visor {
-  position: absolute;
-  width: 50%;
-  left: 50%;
-  transform: translateX(-50%);
-  height: 30px;
-  border-right: 2px solid rgba(white, .2);
-  border-left: 2px solid rgba(white, .2);
-  &--top {
-    border-top: 2px solid rgba(white, .2);
-    top: 80px;
-  }
-  &--down {
-    border-bottom: 2px solid rgba(white, .2);
-    bottom: 80px;
-  }
-}
-
-.vector {
-  opacity: .5;
-  position: absolute;
-  &:first-child {
-    left: 40px;
-    top: 40px;
-    transform: scaleY(-1);
-  }
-  &:last-child {
-    left: 40px;
-    bottom: 40px;
-  }
-}
-
-.square {
-  width: 10px;
-  height: 20px;
-  border: 2px solid orange;
-  display: inline-block;
-  transform: skew(-15deg);
-  margin-right: 2px;
-  margin-top: 40px;
-}
-
-.line {
-  width: 120px;
-  display: inline-block;
-  border-bottom: 5px solid orange;
-  position: absolute;
-  top: 90px;
-  right: 25px;
-  transform: skew(-15deg);
-}
-</style>
